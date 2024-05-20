dd5f15923cd82e387d8274f8b37a6c5cc174d374
Tymofii Pashkevych <tymofii.pashkevych@gmail.com>
2024-05-20 05:03:22 +0300

    Cleanup global.css and tailwind.config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,14 @@ const config: Config = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'hero-image': "url('/hero-bg.jpg')",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+    },
   },
   plugins: [],
 };

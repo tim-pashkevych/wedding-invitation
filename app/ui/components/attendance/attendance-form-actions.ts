059022c894ba50c66f90ce1b97c5d11440321c44
Tymofii Pashkevych <tymofii.pashkevych@gmail.com>
2024-06-27 19:08:09 -0700

    SendGrid integration: adding support for multiple recipients

--- app/ui/components/attendance/attendance-form-actions.ts
+++ app/ui/components/attendance/attendance-form-actions.ts
@@ -29,7 +29,7 @@ export async function sendEmail(data: IAttendanceSubmission) {
   );
   const totalKids = rows.reduce((total, row) => total + Number(row.kids), 0);
 
-  const totalStatistics = `TOTAL:<br/><strong>Guests:</strong> ${totalGuests}<br/><strong>Adults:</strong> ${totalAdults}<br/><strong>Kids:</strong> ${totalKids}<br/><br/>`;
+  const totalStatistics = `TOTAL:<br/><br/><strong>Guests:</strong> ${totalGuests}<br/><strong>Adults:</strong> ${totalAdults}<br/><strong>Kids:</strong> ${totalKids}<br/><br/>`;
   const submissionHtml = `You have received a new confirmation of the wedding addending.<br/><br/><strong>Name:</strong> ${data.name}<br/><strong>Adults:</strong> ${data.adults}<br/><strong>Kids:</strong> ${data.kids}<br/><br/>`;
   const allSubmissionsHtml = `All submissions:<br/>
     <table border="1" cellpadding="10">
@@ -40,17 +40,28 @@ export async function sendEmail(data: IAttendanceSubmission) {
         <td><strong>Kids</strong></td>
         <td><strong>Date</strong></td>
       </tr>
-      ${rows.map((row, index) => `<tr><td>${++index}</td><td>${row.name}</td><td>${row.adults}</td><td>${row.kids}</td><td>${row.date}</td></tr>`)}
+      ${rows.map((row, index) => `<tr><td>${++index}</td><td>${row.name}</td><td>${row.adults}</td><td>${row.kids}</td><td>${row.date}</td></tr>`).join('')}
     </table>
     <br/><br/>`;
 
+  const sendTo = process.env.SENDGRID_SEND_TO
+    ? process.env.SENDGRID_SEND_TO.split(',').map((emailAddress) => ({
+        email: emailAddress.trim(),
+        name: '',
+      }))
+    : 'tim.pashkevych@gmail.com';
+
   const msg = {
-    to: process.env.SENDGRID_SEND_TO ?? 'tim.pashkevych@gmail.com',
-    from: process.env.SENDGRID_SEND_FROM ?? 'lumenstudiollc@gmail.com',
+    to: sendTo,
+    from: {
+      email: process.env.SENDGRID_SEND_FROM ?? 'lumenstudiollc@gmail.com',
+      name: 'Sokolov.Wedding Admin',
+    },
     subject: 'New confirmation of the wedding attending',
     text: `You have received a new confirmation of the wedding addending.\n\nName: ${data.name}\nAdults: ${data.adults}\nKids: ${data.kids}\n\n Sent by Sokolow.Wedding`,
-    html: `${submissionHtml} ${allSubmissionsHtml} ${totalStatistics} Sent by <a href="https://sokolov.wedding">Sokolow.Wedding</a>`,
+    html: `${submissionHtml} ${totalStatistics} ${allSubmissionsHtml} Sent by <a href="https://sokolov.wedding">Sokolow.Wedding</a>`,
   };
+
   await sgMail
     .send(msg)
     .then(() => {
@@ -59,4 +70,32 @@ export async function sendEmail(data: IAttendanceSubmission) {
     .catch((error) => {
       console.error(error);
     });
+
+  // const sendToEmails = process.env.SENDGRID_SEND_TO
+  //   ? process.env.SENDGRID_SEND_TO.split(',')
+  //   : ['tim.pashkevych@gmail.com'];
+
+  // await Promise.all(
+  //   sendToEmails.map(async (sendTo) => {
+  //     const msg = {
+  //       to: sendTo.trim(),
+  //       from: {
+  //         email: process.env.SENDGRID_SEND_FROM ?? 'lumenstudiollc@gmail.com',
+  //         name: 'Sokolov.Wedding Admin',
+  //       },
+  //       subject: 'New confirmation of the wedding attending',
+  //       text: `You have received a new confirmation of the wedding addending.\n\nName: ${data.name}\nAdults: ${data.adults}\nKids: ${data.kids}\n\n Sent by Sokolow.Wedding`,
+  //       html: `${submissionHtml} ${totalStatistics} ${allSubmissionsHtml} Sent by <a href="https://sokolov.wedding">Sokolow.Wedding</a>`,
+  //     };
+
+  //     await sgMail
+  //       .send(msg)
+  //       .then(() => {
+  //         console.log('Email sent');
+  //       })
+  //       .catch((error) => {
+  //         console.error(error);
+  //       });
+  //   }),
+  // );
 }
